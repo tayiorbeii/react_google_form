e99a5497b0bd8191f9ec655e0ed3eabd250e3c5c
Taylor Bell <taylorbell@gmail.com>
2018-04-15 01:19:06 -0600

    working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
 import React, { Component } from 'react';
 import axios from 'axios'
+import validator from 'email-validator'
 
 // Directions here: https://medium.com/@dmccoy/how-to-submit-an-html-form-to-google-sheets-without-google-forms-b833952cc175
 // Google Form URL
 // https://script.google.com/macros/s/AKfycbzPrz7PNhuWup2MH-8BaKB-errFJWmM8xGjdbY6zqqFncKY_DHr/exec
 
+const formUrl = 'https://script.google.com/macros/s/AKfycbzPrz7PNhuWup2MH-8BaKB-errFJWmM8xGjdbY6zqqFncKY_DHr/exec'
+const ThankYou = () => <div>Thank You!</div>
+
+class FormTwo extends Component {
+  render() {
+    const showEmailError = this.emailNode && !this.props.emailIsValid
+    return (
+      <form
+        onChange={this.props.onChange}
+        onSubmit={this.props.onSubmit}
+      >
+        <label>
+          Name <input type='text' name='name' />
+        </label>
+
+        <label>
+          Email <input type='text' name='email' ref={node => (this.emailNode = node)}/>
+        </label>
+
+        <button disabled={!this.props.emailIsValid}>
+          Submit
+        </button>
+
+        {showEmailError ? <div>Invalid Email</div> : ''}
+
+      </form>
+    )
+  }
+}
+
 class App extends Component {
-  state = { submitted: false }
+  state = {
+    name: '',
+    email: '',
+    emailIsValid: false,
+    submitted: false
+  }
+
+  validateEmail = () => {
+    this.setState({emailIsValid: validator.validate(this.state.email)})
+  }
+
+  updateValues = (e) => {
+    e.preventDefault()
+    this.setState({[e.target.name]: e.target.value})
+    this.validateEmail()
+  }
 
   handleSubmit = (e) => {
     e.preventDefault()
-    const formUrl = 'https://script.google.com/macros/s/AKfycbzPrz7PNhuWup2MH-8BaKB-errFJWmM8xGjdbY6zqqFncKY_DHr/exec'
-    const name = encodeURIComponent(this.inputName.value)
-    const email = encodeURIComponent(this.inputEmail.value)
+
+    const { name, email } = this.state
 
     axios({
       method: 'get',
-      url: `${formUrl}?name=${name}&email=${email}`
+      url: `${formUrl}?name=${encodeURIComponent(name)}&email=${encodeURIComponent(email)}`
     })
 
     this.setState({submitted: true})
@@ -25,28 +70,15 @@ class App extends Component {
   render() {
     const { submitted } = this.state
 
-    const NameAndEmailForm = () => (
-      <form onSubmit={this.handleSubmit}>
-        <label>Name
-        <input type='text' name='name' ref={node => this.inputName = node} />
-        </label>
-
-        <label>Email
-        <input type='text' name='email' ref={node => this.inputEmail = node} />
-        </label>
-        
-        <button type='submit'>Submit</button>
-
-      </form>
-    )
-
-    const ThankYou = () => <div>Thank You!</div>
-
     return (
       <div>
         {submitted
           ? <ThankYou />
-          : <NameAndEmailForm />
+          : <FormTwo
+              onChange={this.updateValues}
+              onSubmit={this.handleSubmit}
+              emailIsValid={this.state.emailIsValid}
+            />
         }
       </div>
     );
